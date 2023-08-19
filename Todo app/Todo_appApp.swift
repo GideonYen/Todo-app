a263ce7cbacdd133df8f80d3bcacb1fbcdff5580
Gideon Yen <gideon.yen20@gmail.com>
2023-08-19 19:34:26 +0800

    Initial Commit

---
+++ Todo app/Todo_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_appApp.swift
+//  Todo app
+//
+//  Created by Sapphire Blue on 19/8/23.
+//
+
+import SwiftUI
+
+@main
+struct Todo_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
